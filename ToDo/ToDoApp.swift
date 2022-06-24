07d563881d136f0a917af78c3dd8bf56e2d654ae
Eugene Chopenko <chopenko@gmail.com>
2022-06-24 10:30:22 +0300

    Initial Commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by Eugene Chopenko on 24.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
